c3a94fdfe56f76d8a2a4762e960362359fbe1022
Apace <apace100@web.de>
2020-02-29 22:04:43 +0100

    Added Ring of Restoration

---
+++ Assets/Game/Battle/Items/RestoreRing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestoreRing : Equipment
+{
+
+	public RestoreRing(int id, string name) : base(id, name, "You restore twice as much health when an enemy is killed.") {
+		
+	}
+
+	public override void OnEquipped(Pawn pawn) {
+		PlayerPawn player = pawn as PlayerPawn;
+		if(player != null) {
+			player.EndOfBattleRestoration.AddModifier(new AttributeModifier(GetName(), AttributeModifier.Operation.MultiplyTotal, 2));
+		}
+	}
+
+	public override void OnUnequipped(Pawn pawn) {
+		PlayerPawn player = pawn as PlayerPawn;
+		if(player != null) {
+			player.EndOfBattleRestoration.RemoveModifier(GetName());
+		}
+	}
+}
