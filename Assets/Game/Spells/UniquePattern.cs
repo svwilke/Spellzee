b4d88aadc1a63b2a01142bb0624642a9a1a810a4
Apace <apace100@web.de>
2020-03-17 14:10:24 +0100

    Allowed AI to correctly identify UniquePattern distance

--- Assets/Game/Spells/UniquePattern.cs
+++ Assets/Game/Spells/UniquePattern.cs
@@ -16,9 +16,18 @@ public class UniquePattern : PatternMatcher {
 	}
 
 	public override void Match(RollContext context) {
+		Reset();
 		rollContext = context;
-		HashSet<Element> uniqueElements = new HashSet<Element>(context.GetElementsRolled());
-		doesMatch = uniqueElements.Count >= uniqueCountRequired;
+		HashSet<Element> uniqueElements = new HashSet<Element>();
+		Element[] rolled = context.GetElementsRolled();
+		for(int i = 0; i < rolled.Length; i++) {
+			if(!uniqueElements.Contains(rolled[i])) {
+				uniqueElements.Add(rolled[i]);
+				matchingDice.Add(i);
+			}
+		}
+		distance = Mathf.Max(0, uniqueCountRequired - uniqueElements.Count);
+		doesMatch = distance == 0;
 	}
 
 	public override ElementDisplay[] GetElementDisplays() {
